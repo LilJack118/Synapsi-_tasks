300e91817f943ff44584b1f0914930a9c236750e
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-11-08 12:29:44 +0100

    'basic_auth'

--- Api/api.py
+++ Api/api.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 import string
+import secrets
 import base64
 import json
 from cipher import encoder, decryptor
@@ -14,27 +15,40 @@ app = FastAPI()
 security = HTTPBasic()
 
 
-@app.get("/users/me")
-def read_current_user(credentials: HTTPBasicCredentials = Depends(security)):
-    return {"username": 'user', "password": '1234'}
-
 
 
 @app.post("/encode/")
-async def encode_message(message: Message):
-    letter_list = [string.printable.index(x) for x in message.Content]
-    encoded_message = encoder(letter_list)
-    encoded_message_bytes = (b''.join([bytes([x]) for x in encoded_message]))
-    encoded = base64.b64encode(encoded_message_bytes)
-    message.Content = encoded
+async def encode_message(message: Message, credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "admin")
+    correct_password = secrets.compare_digest(credentials.password, "1234")
+    if not (correct_username and correct_password):
+        message.Content = 'Not Authorized'
+    else:
+        try:
+            letter_list = [string.printable.index(x) for x in message.Content]
+            encoded_message = encoder(letter_list)
+            encoded_message_bytes = (b''.join([bytes([x]) for x in encoded_message]))
+            encoded = base64.b64encode(encoded_message_bytes)
+            message.Content = encoded
+        except Exception:
+            message.Content = 'Invalid Input'
 
     return message
 
 
 @app.post("/decode/")
-async def decode_message(message: Message):
-    decoded_base64 = base64.b64decode(message.Content)
-    decoded_list = decryptor([x for x in decoded_base64])
-    message.Content = ''.join([string.printable[index] for index in decoded_list])
+async def decode_message(message: Message, credentials: HTTPBasicCredentials = Depends(security)):
+
+    correct_username = secrets.compare_digest(credentials.username, "admin")
+    correct_password = secrets.compare_digest(credentials.password, "1234")
+    if not (correct_username and correct_password):
+        message.Content = 'Not Authorized'
+    else:
+        try:
+            decoded_base64 = base64.b64decode(message.Content)
+            decoded_list = decryptor([x for x in decoded_base64])
+            message.Content = ''.join([string.printable[index] for index in decoded_list])
+        except Exception:
+            message.Content = 'Invalid Input'
 
     return message
