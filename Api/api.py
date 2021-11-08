78f61d13b46e0f03c3855d53bf22e93151580402
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-11-08 11:35:19 +0100

    'api_encoder'

--- Api/api.py
+++ Api/api.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
+import string
+import base64
+import json
+from cipher import encoder, decryptor
 
 
 class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
+    Message: str
 
 app = FastAPI()
 
@@ -17,6 +17,24 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/create/")
+@app.post("/encode/")
 async def create(item: Item):
+    letter_list = [string.printable.index(x) for x in item.Message]
+    encoded_message = encoder(letter_list)
+    encoded_message_bytes = (b''.join([(x).to_bytes(2, byteorder='big') for x in encoded_message]))
+    encoded = base64.b64encode(encoded_message_bytes)
+    item.Message = encoded
+
+    return item
+
+
+@app.post("/decode/")
+async def create(item: Item):
+    decoded_base64 = base64.b64decode(item.Message)
+    decoded_list = decryptor(decoded_base64)
+    decoded_message = ''
+    for letter_index in decoded_list:
+        decoded_message +=string.printable[letter_index]
+    print(decoded_list)
+
     return item
--- Api/cipher.py
+++ Api/cipher.py
@@ -1,7 +1,10 @@
 
 import string
 import random
+import base64
+import json
 
+l = 'AFcAXABLABYAHwATACsAJAAuABEASw=='
 
 list_s = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96] 
 
@@ -12,7 +15,7 @@ list_s = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
 # random.shuffle(list_s)
 # print(list_s)
 
-def rotator_one(letter_index_list):
+def encoder(letter_index_list):
     encrypted_list = []
 
     input_rotator_1 = [64, 57, 30, 80, 89, 52, 6, 96, 34, 28, 71, 19, 60, 35, 16, 9, 88, 79, 7, 78, 51, 40, 87, 55, 14, 22, 95, 17, 68, 84, 92, 75, 49, 23, 11, 61, 21, 32, 41, 2, 3, 50, 93, 74, 69, 39, 56, 8, 4, 90, 10, 31, 67, 70, 46, 36, 91, 86, 5, 24, 77, 85, 76, 59, 0, 62, 47, 18, 37, 83, 44, 72, 25, 43, 73, 13, 81, 66, 58, 42, 65, 53, 1, 12, 15, 48, 38, 63, 26, 29, 45, 54, 82, 27, 33, 94, 20] 
@@ -36,7 +39,7 @@ def rotator_one(letter_index_list):
     return encrypted_list
 
 
-print(rotator_one([61,12,14,61,61]))
+print(encoder([61,12,14,61,61]))
 
 
 
@@ -64,9 +67,16 @@ def decryptor(letter_index_list):
         output_1 = input_rotator_2.index(output_rotator_2[output_2])
 
         letter = input_rotator_1.index(output_rotator_1[output_1])
-        decrypted_list.append(letter)
+        decrypted_list.insert(0,letter)
 
     return decrypted_list
 
 
 print(decryptor([34, 21, 37,43]))
+
+
+encoded_list = encoder([61,12,14,61,61])
+json_encoded_list = json.dumps(encoded_list)
+json_encoded = str.encode(json_encoded_list)
+encoded = base64.b64encode(json_encoded)
+print(encoded)
